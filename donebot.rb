5b0c9931a0c3741d61f4749222b0f278aafd68eb
Zach Feldman <zach@nycda.com>
2015-10-27 18:44:29 -0400

    Initial commit.

---
+++ donebot.rb
@@ -0,0 +1,31 @@
+require 'slack-ruby-bot'
+
+COUNTER = 0
+
+module DoneBot
+  class App < SlackRubyBot::App
+  end
+
+  class Done < SlackRubyBot::Commands::Base
+    def self.call(client, data, _match)
+      COUNTER += 1
+      finished_sayings = ["Great job!", "Nice work.", "Keep at it.", "Do, or do not. There is no try!"]
+      client.message text: finished_sayings[(rand*4).ceil-1], channel: data.channel
+    end
+  end
+
+  class AmountDone < SlackRubyBot::Commands::Base
+    def self.call(client, data, _match)
+      client.message text: "Counter is currently at #{COUNTER}.", channel: data.channel
+    end
+  end
+
+  class Reset < SlackRubyBot::Commands::Base
+    def self.call(client, data, _match)
+      COUNTER = 0
+      client.message text: "Counter reset to 0.", channel: data.channel
+    end
+  end
+end
+
+DoneBot::App.instance.run
