077008f73469f8cea8f982d6c31542a0760b721e
Zach Feldman <zach@nycda.com>
2015-10-28 16:07:01 -0400

    Fix things up.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source 'http://rubygems.org'
 
-gem 'slack-ruby-bot'
+gem 'slack-ruby-bot', git: "https://github.com/zachfeldman/slack-ruby-bot.git"
--- donebot.rb
+++ donebot.rb
@@ -7,30 +7,45 @@ module DoneBot
   end
 
   class Counter < SlackRubyBot::Commands::Base
-    match /^Setup a new counter for (?<counter_name>\w*)$/ do |data, match|
+    match /^Setup a counter for (?<counter_name>\w*)$/ do |client, data, match|
       COUNTERS[match[:counter_name]] = 0
-      send_message data.channel, "I setup a new counter for #{match[:counter_name]}."
+      send_message client, data.channel, "I setup a new counter for #{match[:counter_name]}."
     end
   end
 
   class Done < SlackRubyBot::Commands::Base
-    match /^I'm done with (?<counter_name>\w*)$/ do |data, match|
-      COUNTERS[match[:counter_name]] += 1
+    match /^Done with (?<counter_name>\w*)$/ do |client, data, match|
       finished_sayings = ["Great job!", "Nice work.", "Keep at it.", "Do, or do not. There is no try!"]
-      send_message data.channel, finished_sayings[(rand*4).ceil-1]
+      if COUNTERS[match[:counter_name]]
+        COUNTERS[match[:counter_name]] += 1     
+        response = finished_sayings[(rand*4).ceil-1] 
+      else
+        response = "Sorry, there's no counter with that name. If you'd like to set one up, just say, \"Setup a counter for #{match[:counter_name]}\""
+      end
+      send_message client, data.channel, response
     end
   end
 
   class AmountDone < SlackRubyBot::Commands::Base
-    match /^How many people are done with (?<counter_name>\w*)$/ do |data, match|
-      send_message data.channel, "#{COUNTERS[match[:counter_name]]} are done."
+    match /^How many are done with (?<counter_name>\w*)$/ do |client, data, match|
+      if COUNTERS[match[:counter_name]]
+        response = "#{COUNTERS[match[:counter_name]]} are done."
+      else
+        response = "Sorry, there's no counter with that name. If you'd like to set one up, just say, \"Setup a counter for #{match[:counter_name]}\""
+      end
+      send_message client, data.channel, response
     end
   end
 
   class Reset < SlackRubyBot::Commands::Base
-    match /^Reset the (?<counter_name>\w*)$/ do |data, match|
-      COUNTERS[match[:counter_name]] = 0
-      send_message data.channel, "#{COUNTERS[match[:counter_name]]} set to 0."
+    match /^Reset the (?<counter_name>\w*)$/ do |client, data, match|
+      if COUNTERS[match[:counter_name]]
+        COUNTERS[match[:counter_name]] = 0
+        response = "#{COUNTERS[match[:counter_name]]} has been reset to 0."
+      else
+        response = "Sorry, there's no counter with that name. If you'd like to set one up, just say, \"Setup a counter for #{match[:counter_name]}\""
+      end
+      send_message client, data.channel, response
     end
   end
 end
