57a2f99c23a791edb9a8801ee8ad46823aed3f64
Zach Feldman <zach@nycda.com>
2015-10-28 15:20:38 -0400

    More testing.

--- donebot.rb
+++ donebot.rb
@@ -1,20 +1,20 @@
 require 'slack-ruby-bot'
 
-COUNTER = {}
+COUNTERS = {}
 
 module DoneBot
   class App < SlackRubyBot::App
   end
 
-  class NewCounter < SlackRubyBot::Commands::Base
-    match /^Setup a new counter for (?<counter_name>\w*)\?$/ do |client, data, match|
+  class Counter < SlackRubyBot::Commands::Base
+    match /^Setup a new counter for (?<counter_name>\w*)$/ do |client, data, match|
       COUNTERS[match[:counter_name]] = 0
       send_message client, data.channel, "I setup a new counter for #{match[:counter_name]}."
     end
   end
 
   class Done < SlackRubyBot::Commands::Base
-    match /^I'm done with (?<counter_name>\w*)\?$/ do |client, data, match|
+    match /^I'm done with (?<counter_name>\w*)$/ do |client, data, match|
       COUNTERS[match[:counter_name]] += 1
       finished_sayings = ["Great job!", "Nice work.", "Keep at it.", "Do, or do not. There is no try!"]
       send_message client, data.channel, finished_sayings[(rand*4).ceil-1]
@@ -22,13 +22,13 @@ module DoneBot
   end
 
   class AmountDone < SlackRubyBot::Commands::Base
-    match /^How many people are done with (?<counter_name>\w*)\?$/ do |client, data, match|
+    match /^How many people are done with (?<counter_name>\w*)$/ do |client, data, match|
       send_message client, data.channel, "#{COUNTERS[match[:counter_name]]} are done."
     end
   end
 
   class Reset < SlackRubyBot::Commands::Base
-    match /^Reset the (?<counter_name>\w*) counter$/ do |client, data, match|
+    match /^Reset the (?<counter_name>\w*)$/ do |client, data, match|
       COUNTERS[match[:counter_name]] = 0
       send_message client, data.channel, "#{COUNTERS[match[:counter_name]]} set to 0."
     end
